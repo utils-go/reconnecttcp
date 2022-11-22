0862412d8b6a58175b2686dc3c6b87952f5afe47
李双全 <294388344@qq.com>
2022-11-22 16:08:32 +0800

    add reconnecttcpnew

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lishuangquan1987/reconnecttcp
 
-go 1.17
+go 1.19
+
+require github.com/utils-go/concurrentlist v0.0.3
--- reconnecttcp_test.go
+++ reconnecttcp_test.go
@@ -2,48 +2,65 @@ package reconnecttcp
 
 import (
 	"fmt"
-	"net"
-	"strconv"
 	"testing"
 	"time"
 )
 
-func TestReConnection(t *testing.T) {
-	addr := "127.0.0.1:5000"
-	client := NewReconnectTcp(addr)
-	go func(r *ReconnectTcp) {
-		for data := range r.Read() {
-			fmt.Printf("client recv:%s\n", data)
-		}
-	}(client)
+//func TestReConnection(t *testing.T) {
+//	addr := "127.0.0.1:5000"
+//	client := NewReconnectTcp(addr)
+//	go func(r *ReconnectTcp) {
+//		for data := range r.Read() {
+//			fmt.Printf("client recv:%s\n", data)
+//		}
+//	}(client)
+//
+//	go func(r *ReconnectTcp) {
+//		for {
+//			r.Write([]byte("message from client"))
+//			time.Sleep(time.Millisecond * 10)
+//		}
+//	}(client)
+//
+//	l, err := net.Listen("tcp", addr)
+//	if err != nil {
+//		t.Fatalf("listen %s fail:%v\n", addr, err)
+//		return
+//	}
+//
+//	go func(listener net.Listener) {
+//		for i := 0; i < 10; i++ {
+//			conn, err := l.Accept()
+//			if err != nil {
+//				fmt.Printf("accept fail:%v\n", err)
+//				continue
+//			}
+//
+//			conn.Write([]byte("hello" + strconv.Itoa(i)))
+//			time.Sleep(time.Millisecond * 100)
+//			conn.Close()
+//		}
+//	}(l)
+//
+//	time.Sleep(time.Second * 5)
+//
+//}
 
-	go func(r *ReconnectTcp) {
-		for {
-			r.Write([]byte("message from client"))
-			time.Sleep(time.Millisecond * 10)
+func TestReConnectionNew(t *testing.T) {
+	addr := "127.0.0.1:5001"
+	client := NewReconnectTcpNew(addr)
+	go func() {
+		data := client.Read()
+		if data != nil {
+			fmt.Printf("接收:%s\n", string(data))
 		}
-	}(client)
-
-	l, err := net.Listen("tcp", addr)
-	if err != nil {
-		t.Fatalf("listen %s fail:%v\n", addr, err)
-		return
+		time.Sleep(200 * time.Millisecond)
+	}()
+	for {
+		fmt.Println("请输入要发送的内容：")
+		var str string
+		fmt.Scanln(&str)
+		client.Write([]byte(str))
 	}
-
-	go func(listener net.Listener) {
-		for i := 0; i < 10; i++ {
-			conn, err := l.Accept()
-			if err != nil {
-				fmt.Printf("accept fail:%v\n", err)
-				continue
-			}
-
-			conn.Write([]byte("hello" + strconv.Itoa(i)))
-			time.Sleep(time.Millisecond * 100)
-			conn.Close()
-		}
-	}(l)
-
-	time.Sleep(time.Second * 5)
 
 }
